e95db3e4bc5928f54bfb832d7523ccc96d0a56cb
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2017-06-21 00:58:50 -0400

    Add game introduction

--- termhacker.py
+++ termhacker.py
@@ -1,5 +1,16 @@
+import time
+import util
+
 def main():
-    pass
+    show_intro()
+
+def show_intro():
+    util.clear()
+    util.dprint("Terminal hacking in progress", 0.05)
+    util.dprint('...', 0.5)
+    time.sleep(0.1)
+    print(" Password required\n")
+    time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
