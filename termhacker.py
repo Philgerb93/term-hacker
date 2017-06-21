8e982026546c0cf5e88710053e92961247719b7a
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2017-06-21 15:22:43 -0400

    Complete game intro

--- termhacker.py
+++ termhacker.py
@@ -6,18 +6,10 @@ def main():
     words = util.get_words(4)
     answer = random.choice(words)
     grid = set_grid(words)
+    attempts = 4
 
     util.set_color('green')
-    show_intro()
-    show_grid(grid)
-
-def show_intro():
-    util.clear()
-    util.dprint("Terminal hacking in progress", 0.05)
-    util.dprint('...', 0.5)
-    time.sleep(0.1)
-    print(" Password required\n")
-    time.sleep(0.5)
+    show_intro(attempts, grid)
 
 def set_grid(words):
     COLS = 30
@@ -39,7 +31,21 @@ def set_grid(words):
 
     return grid
 
-def show_grid(grid):
+def show_intro(attempts, grid):
+    util.clear()
+    util.dprint("Terminal hacking in progress", 0.05)
+    util.dprint('...', 0.5)
+    time.sleep(0.1)
+    print(" Password required\n")
+    time.sleep(0.5)
+
+    util.dprint("Attempts remaining : " + str(attempts) + '\n', 0.05)
+    show_grid_intro(grid)
+
+    util.dprint("Decrypting", 0.05)
+    util.dprint('...\n', 0.5)
+
+def show_grid_intro(grid):
     print('=' * len(grid[0]))
     for line in grid:
         util.dprint(line + '\n', 0.005)
