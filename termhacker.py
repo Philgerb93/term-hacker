e61d5839d4259c3cf7abbc74d443d872cac0a27c
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2017-06-22 01:30:45 -0400

    Add replay option at the end of a game

--- termhacker.py
+++ termhacker.py
@@ -1,31 +1,34 @@
 import random
 import time
 import util
+import sys
 
 ROWS = 15
 COLS = 30
 
 def main():
-    words = util.get_words(4, ROWS)
-    answer = random.choice(words)
-    grid = set_grid(words)
-    game_over = False
-    attempts = 4
+    playing = True
+    while playing:
+        words = util.get_words(4, ROWS)
+        answer = random.choice(words)
+        grid = set_grid(words)
+        game_over = False
+        attempts = 4
 
-    util.set_color('green')
-    show_intro(attempts, grid)
+        util.set_color('green')
+        show_intro(attempts, grid)
 
-    while not game_over and attempts > 0:
-        reprint(attempts, grid)
-        user_input = input('> ').upper()
+        while playing and not game_over and attempts > 0:
+            reprint(attempts, grid)
+            user_input = input('> ').upper()
 
-        if len(user_input) == 0:
-            game_over = confirm_exit(attempts, grid)
-        elif user_input in words:
-            game_over = validate_input(user_input, answer, words, grid)
-            attempts -= 1
+            if len(user_input) == 0:
+                confirm_exit(attempts, grid)
+            elif user_input in words:
+                game_over = validate_input(user_input, answer, words, grid)
+                attempts -= 1
 
-    show_results(attempts, answer, words, grid)
+        playing = end_game(attempts, answer, words, grid)
 
 def set_grid(words):
     DATA = ['!', '@', '#', '$', '%', '?', '&', '*',
@@ -66,25 +69,24 @@ def show_grid_intro(grid):
         util.dprint(line + '\n', 0.005)
     print('=' * COLS)
 
-def reprint(attempts, grid, answer_index=None):
+def reprint(attempts, grid):
     util.clear()
     print("Terminal hacking in progress... Password required\n")
     print("Attempts remaining : " + str(attempts))
-    show_grid(grid, answer_index)
+    show_grid(grid)
     print('')
 
-def show_grid(grid, answer_index):
+def show_grid(grid):
     is_word = False
 
     print('=' * COLS)
-    for x in range(len(grid)):
-        if answer_index != None and answer_index == x:
+    for line in grid:
+        if len(line) > COLS + 2:
             word_highlight = 'yellow'
-            grid[x] += " <= PASSWORD"
         else:
-            word_highlight = 'light green' if len(grid[x]) == COLS else 'red'
+            word_highlight = 'light green' if len(line) == COLS else 'red'
 
-        for char in grid[x]:
+        for char in line:
             if not is_word and char.isalpha():
                 util.set_color(word_highlight)
                 is_word = True
@@ -102,13 +104,11 @@ def show_grid(grid, answer_index):
 def confirm_exit(attempts, grid):
     while True:
         reprint(attempts, grid)
-        print("Quit the game? (Y/N)")
-
+        print("Quit the game? (Y/N) ", end='')
         user_input = input('> ').lower()
+
         if user_input == 'y':
-            return True
-        elif user_input == 'n':
-            return False
+            sys.exit()
 
 def validate_input(user_input, answer, words, grid):
     if user_input == answer:
@@ -129,12 +129,22 @@ def calculate_matches(word, answer):
 
     return matches
 
-def show_results(attempts, answer, words, grid):
-    reprint(attempts, grid, words.index(answer))
-    if attempts > 0:
-        util.dprint("Hacking SUCCESSFUL\n", 0.05)
-    else:
-        util.dprint("Hacking FAILED\n", 0.05)
+def end_game(attempts, answer, words, grid):
+    grid[words.index(answer)] += " <= PASSWORD"
+
+    while True:
+        reprint(attempts, grid)
+        if attempts > 0:
+            print("Hacking SUCCESSFUL")
+        else:
+            print("Hacking FAILED")
+        
+        print("\nPlay again? (Y/N) ", end='')
+        user_input = input('> ').lower()
+        if user_input == 'y':
+            return True
+        elif user_input == 'n':
+            return False
 
 if __name__ == '__main__':
     main()
